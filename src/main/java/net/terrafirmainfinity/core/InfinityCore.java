249f7e1017e95f93746bf580d2ed4161444ffb5b
strangelyng <170715141+strangelyng@users.noreply.github.com>
2026-08-09 00:20:07 -0700

    Added dependencies: Create, Ponder, Flywheel

--- src/main/java/net/terrafirmainfinity/core/InfinityCore.java
+++ src/main/java/net/terrafirmainfinity/core/InfinityCore.java
@@ -1,6 +1,8 @@
 package net.terrafirmainfinity.core;
 
+import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -12,22 +14,27 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(InfinityCore.MOD_ID)
-public class InfinityCore
-{
+public class InfinityCore {
     public static final String MOD_ID = "tficore";
     public static final Logger LOGGER = LogUtils.getLogger();
-
-    public static ResourceLocation id(String path) {
-        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
-    }
+    public static GTRegistrate INFINITY_REGISTRATE = GTRegistrate.create(MOD_ID);
 
     public InfinityCore(IEventBus bus, ModContainer mod) {
         bus.addListener(this::commonSetup);
+        bus.addListener(this::clientSetup);
 
         mod.registerConfig(ModConfig.Type.COMMON, InfinityConfig.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
 
+    }
+
+    private void clientSetup(FMLClientSetupEvent event) {
+
+    }
+
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 }
---
+++ src/main/java/net/terrafirmainfinity/core/InfinityCoreGTAddon.java
@@ -0,0 +1,21 @@
+package net.terrafirmainfinity.core;
+
+import com.gregtechceu.gtceu.api.addon.GTAddon;
+import com.gregtechceu.gtceu.api.addon.IGTAddon;
+import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
+
+@GTAddon(InfinityCore.MOD_ID)
+public class InfinityCoreGTAddon implements IGTAddon
+{
+    @Override
+    public GTRegistrate getRegistrate()
+    {
+        return InfinityCore.INFINITY_REGISTRATE;
+    }
+
+    @Override
+    public void gtInitComplete()
+    {
+        InfinityCore.LOGGER.info("InfinityCoreGTAddon has loaded!");
+    }
+}
