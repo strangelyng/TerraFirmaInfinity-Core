ec55f0d9fa6e06cab58c4c47c3091c75cbf9f50a
strangelyng <170715141+strangelyng@users.noreply.github.com>
2026-08-13 23:08:09 -0700

    Add hot overlay textures for dull and metallic icon sets - Fix machine lang

--- src/main/java/net/terrafirmainfinity/core/common/data/InfinityMachines.java
+++ src/main/java/net/terrafirmainfinity/core/common/data/InfinityMachines.java
@@ -3,24 +3,26 @@ package net.terrafirmainfinity.core.common.data;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.machine.MachineDefinition;
 import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
+import com.gregtechceu.gtceu.common.mui.GTSingleblockMachinePanels;
 import net.minecraft.network.chat.Component;
 import net.terrafirmainfinity.core.InfinityCore;
 
+import static com.gregtechceu.gtceu.api.GTValues.*;
 import static com.gregtechceu.gtceu.common.data.machines.GTMachineUtils.*;
-import static com.gregtechceu.gtceu.common.mui.GTSingleblockMachinePanels.*;
 import static net.terrafirmainfinity.core.InfinityCore.REGISTRATE;
 
 public class InfinityMachines {
     public static final MachineDefinition[] HPHT_VACUUM_PRESS = registerTieredMachines(REGISTRATE, "hpht_vacuum_press",
-            (holder, tier) -> new SimpleTieredMachine(holder, tier, defaultTankSizeFunction),
-            (tier, builder) -> builder.recipeType(InfinityRecipeTypes.HPHT_VACUUM_PRESS_RECIPE)
-                    .ui(GENERAL_MACHINE)
+            SimpleTieredMachine::new, (tier, builder) -> builder
+                    .langValue("%s HPHT Vacuum Press %s".formatted(VLVH[tier], VLVT[tier]))
+                    .recipeType(InfinityRecipeTypes.HPHT_VACUUM_PRESS_RECIPE)
+                    .ui(GTSingleblockMachinePanels.GENERAL_MACHINE)
+                    .workableTieredHullModel(InfinityCore.id("block/machines/hpht_vacuum_press"))
                     .tooltipBuilder((stack, list) -> {
                         list.add(Component.translatable("tfi.machine.hpht_vacuum_press.tooltip"));
                     })
                     .tooltips(workableTiered(tier, GTValues.V[tier], GTValues.V[tier] * 64, InfinityRecipeTypes.HPHT_VACUUM_PRESS_RECIPE,
                             defaultTankSizeFunction.applyAsInt(tier), true))
-                    .workableTieredHullModel(InfinityCore.id("block/overlay/machine/hpht_vacuum_press"))
                     .register(),
             ELECTRIC_TIERS);
 
